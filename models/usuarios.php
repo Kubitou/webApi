fb658a58c14aea3ffb6ecbcb07ef507a5287ee86
Kubitou <leo23kubo@gmail.com>
2025-05-27 08:34:57 -0300

    Implementação de recursos

--- models/usuarios.php
+++ models/usuarios.php
@@ -13,11 +13,58 @@
         public function __construct($db){
             $this->conn = $db;
         }
+
         function read(){
             $query = "select id, nome, email, ra, celular from $this->table_name order by nome asc";
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt;
+        }
+
+        function readOne() {
+            $query = "select id, nome, email, ra, celular from $this->table_name where id = :id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':id',$this->id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->name = $row['nome'];
+            $this->email = $row['email'];
+            $this->ra = $row['ra'];
+            $this->celular = $row['celular'];
+        }
+
+        function create(){
+            $retorno = false;
+            $query = "insert into $this->table_name (nome, email, ra, senha, celular) values (:nome, :email, :ra, :senha, :celular)";
+            $stmt = $this->conn->prepare($query);
+            $this->senha = password_hash($this->senha, PASSWORD_BCRYPT);
+            $stmt->bindParam(":nome", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":ra", $this->ra);
+            $stmt->bindParam(":senha", $this->senha);
+            $stmt->bindParam(":celular", $this->celular);
+            if($stmt->execute()){
+                $retorno = true;
+            }
+            return $retorno;
+        }
+
+        function update(){
+            $query = "update $this->table_name set nome=:nome, email=:email, ra=:ra, celular=:celular where id=:id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":nome", $this->name);
+            $stmt->bindParam(":email", $this->email);
+            $stmt->bindParam(":ra", $this->ra);
+            $stmt->bindParam(":celular", $this->celular);
+            return $stmt->execute();
+        }
+
+        function delete(){
+            $query = "delete from $this->table_name where id=:id";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(":id", $this->id);
+            return $stmt->execute();
         }
     }
 ?>
